501956645cb02287ab545684bb9dd4ea4cc40f6b
Hemlata Bhatt and Kate Goss <kate@>
2018-08-06 13:15:39 -0700

    setup html, js, css, README files

---
+++ calculator/js/scripts.js
@@ -0,0 +1,39 @@
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter amother number:"));
+
+alert(add(number1, number2));
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter amother number:"));
+
+alert(sub(number1, number2));
+
+
+var mul = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter amother number:"));
+
+alert(mul(number1, number2));
+
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter amother number:"));
+
+alert(div(number1, number2))
+;
